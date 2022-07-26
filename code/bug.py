41172a27c296b6f107874ec8bb5e5f54e7d88943
Samuel Machat <35379505+SamuelMachat@users.noreply.github.com>
2022-07-26 21:51:47 +0200

    Repaired speed randomization

--- code/bug.py
+++ code/bug.py
@@ -4,9 +4,9 @@ from random import randint
 
 class Bug(Enemy):
 	image = '../graphics/enemy/run'
-	speed = randint(2,9)
 	attack_damage = 10
 	healing_points = 4
 	def __init__(self,size,x,y):
 		super().__init__(size,x,y)
+		self.speed = randint(2,9)
 		self.rect.y += size - self.image.get_size()[1]
--- code/fly.py
+++ code/fly.py
@@ -4,9 +4,9 @@ from random import randint
 
 class Fly(Enemy):
 	image = '../graphics/fly/run'
-	speed = -randint(2,9)
 	attack_damage = 2
 	healing_points = 3
 	def __init__(self,size,x,y):
 		super().__init__(size,x,y)
+		self.speed = -randint(2,9)
 		self.rect.y += size - self.image.get_size()[1]
