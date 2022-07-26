4a6c86319c878328775307031b1f17e934d3c1de
Samuel Machat <35379505+SamuelMachat@users.noreply.github.com>
2022-07-26 22:23:47 +0200

    Edited default player name to Mr. Croak

--- code/main.py
+++ code/main.py
@@ -35,7 +35,7 @@ def start_the_game():
 menu = pygame_menu.Menu('Hogo Frogo', 400, 300,
                        theme=pygame_menu.themes.THEME_GREEN)
 
-menu.add.text_input('PlayerName: ', default='John Doe')
+menu.add.text_input('PlayerName: ', default='Mr. Croak')
 menu.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
 menu.add.button('Play', start_the_game)
 menu.add.button('Quit', pygame_menu.events.EXIT)
