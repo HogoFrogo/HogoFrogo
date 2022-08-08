9b6210114c27e052428613db79e8c5711b8e5709
Westr <84477698+WestrCZ@users.noreply.github.com>
2022-08-08 21:20:53 +0200

    No level reset
    If player dies on enemy, he won't go from level 0 again.

--- code/game.py
+++ code/game.py
@@ -56,7 +56,6 @@ class Game:
 		if self.cur_health <= 0:
 			self.cur_health = 100
 			self.coins = self.coins-30
-			self.max_level = 0
 			self.overworld = Overworld(0,self.max_level,self.screen,self.create_level)
 			self.status = 'overworld'
 			self.level_bg_music.stop()
