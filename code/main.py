e26a8c6f42ff9785df2591bd689f1144cb5d09f7
Westr <84477698+WestrCZ@users.noreply.github.com>
2022-08-18 01:33:29 +0200

    Volume in main menu music

--- code/main.py
+++ code/main.py
@@ -1,3 +1,4 @@
+from hashlib import new
 from typing_extensions import Self
 import pygame, sys
 import pygame_menu
@@ -15,6 +16,8 @@ clock = pygame.time.Clock()
 game = Game(screen)
 
 main_menu_music = pygame.mixer.Sound('../audio/magnetic_b-ing.mp3')
+music_volume = 0.5
+main_menu_music.set_volume(music_volume)
 
 def start_the_game():
 	player_name = name_text_input.get_value()
@@ -34,6 +37,11 @@ def start_the_game():
 		pygame.display.update()
 		clock.tick(60)
 
+def change_music_volume(new_volume):
+	music_volume = new_volume
+	main_menu_music.set_volume(music_volume)
+
+
 menu = pygame_menu.Menu('Hogo Frogo', 400, 300,
                        theme=pygame_menu.themes.THEME_GREEN)
 
@@ -41,5 +49,6 @@ name_text_input = menu.add.text_input('PlayerName: ', default='Mr. Croak')
 difficulty_input = menu.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)])
 menu.add.button('Play', start_the_game)
 menu.add.button('Quit', pygame_menu.events.EXIT)
+volume_slider = menu.add.range_slider('Volume', music_volume, [0, 1], 1, change_music_volume)
 main_menu_music.play(loops = -1)
 menu.mainloop(screen)
