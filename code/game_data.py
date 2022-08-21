ee7a6bbd2fdc347b8b43b7846888fb1fb069bcd4
Samuel Machat <35379505+SamuelMachat@users.noreply.github.com>
2022-08-21 14:16:24 +0200

    Added hard level options

--- code/game_data.py
+++ code/game_data.py
@@ -15,6 +15,9 @@ level_0 = {
 		'flies': 2,
 		'dragonflies': 0,
 		'wasps': 0,
+		'flies-hard': 1,
+		'dragonflies-hard': 1,
+		'wasps-hard': 2,
 		'goal_image': '../graphics/misc/sad_frog.png'}
 level_1 = {
 		'terrain': '../levels/1/level_1_terrain.csv',
@@ -33,6 +36,9 @@ level_1 = {
 		'flies': 10,
 		'dragonflies': 1,
 		'wasps': 0,
+		'flies-hard': 20,
+		'dragonflies-hard': 1,
+		'wasps-hard': 4,
 		'goal_image': '../graphics/misc/sandwich.png'}
 level_2 = {
 		'terrain': '../levels/2/level_2_terrain.csv',
@@ -51,6 +57,9 @@ level_2 = {
 		'flies': 10,
 		'dragonflies': 3,
 		'wasps': 5, 
+		'flies-hard': 5,
+		'dragonflies-hard': 10,
+		'wasps-hard': 12,
 		'goal_image': '../graphics/character/hat.png'}
 level_3 = {
 		'terrain': '../levels/2/level_2_terrain.csv',
@@ -69,6 +78,9 @@ level_3 = {
 		'flies': 10,
 		'dragonflies': 2,
 		'wasps': 0,
+		'flies-hard': 20,
+		'dragonflies-hard': 10,
+		'wasps-hard': 10,
 		'goal_image': '../graphics/character/hat.png'}
 level_4 = {
 		'terrain': '../levels/2/level_2_terrain.csv',
@@ -87,6 +99,9 @@ level_4 = {
 		'flies': 10,
 		'dragonflies': 2,
 		'wasps': 0,
+		'flies-hard': 20,
+		'dragonflies-hard': 10,
+		'wasps-hard': 10,
 		'goal_image': '../graphics/character/hat.png'}
 level_5 = {
 		'terrain': '../levels/2/level_2_terrain.csv',
@@ -105,6 +120,9 @@ level_5 = {
 		'flies': 10,
 		'dragonflies': 2,
 		'wasps': 0,
+		'flies-hard': 20,
+		'dragonflies-hard': 10,
+		'wasps-hard': 10,
 		'goal_image': '../graphics/character/hat.png'}
 
 levels = {
