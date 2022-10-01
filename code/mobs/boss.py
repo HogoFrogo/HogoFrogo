28c91bad20fe109f98ba8dc0336ecb5273e85cf5
Samuel Machat <35379505+SamuelMachat@users.noreply.github.com>
2022-10-01 12:46:12 +0200

    Update boss.py

--- code/mobs/boss.py
+++ code/mobs/boss.py
@@ -1,16 +1,25 @@
 import pygame 
+from tiles import Tile, StaticTile
 from .enemy import Enemy
 from random import randint
+from player import Player
 
 class Boss(Enemy):
-	images_folder = 'wasp'
+	images_folder = ''
 	attack_damage = 60
 	healing_points = 6
 	minimum_speed = 4
 	maximum_speed = 12
-	boss_type = "none"
+	boss_type = 'none'
 	def __init__(self,boss_type):
 		self.boss_type = boss_type
+		self.images_folder=boss_type
 
-	def trigger_action(self,level):
-		print("Bububu!")
+
+	#def trigger_action(self,level: Level):
+	#	print('Bububu!')
+	#	if self.boss_type == 'flyking':
+	#		self.shoot(level.player)
+	
+	def shoot(tile: Tile):
+		print('bang!')
