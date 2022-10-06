c41c01ec70c292007512faab0e5fd964170d738e
Samuel Machat <35379505+SamuelMachat@users.noreply.github.com>
2022-10-06 22:17:42 +0200

    Add Bullet

--- code/mobs/gangster_frog.py
+++ code/mobs/gangster_frog.py
@@ -1,4 +1,6 @@
-import pygame 
+import pygame
+
+from mobs.bullet import Bullet 
 from .enemy import Enemy
 from random import randint
 
@@ -8,10 +10,18 @@ class GangsterFrog(Enemy):
 	healing_points = 4
 	minimum_speed = 2
 	maximum_speed = 5
+	time_to_shoot_max=60
+	time_to_shoot=0
+	
 	def __init__(self,size,x,y):
 		super().__init__(size,x,y)
+		self.time_to_shoot=0
 
-	def shoot(self,tile):
-		print("bang!")
-        # get coordinates of the other tile
-        # set dirwection of bullet by the coordinates
+	def shoot(self,tile,tile_size):
+		if(self.time_to_shoot==0):
+			print("bang!")
+			# get coordinates of the other tile
+			# set dirwection of bullet by the coordinates
+			# self.time_to_shoot = 60
+			bullet = Bullet(tile_size,self.rect.x,self.rect.y,randint(0,359),12)
+			return bullet
